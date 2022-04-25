8f39acaf2b1a6b4bf3ddcb5a5accee57fa70718a
Alex Younger <alexanderyounger@gmail.com>
2022-04-25 08:50:24 -0400

    style header

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,17 @@
 import styled from 'styled-components';
 import './App.css';
+import Header from './components/Header';
+import ViewController from './components/controller/ViewController';
 
 const AppContainer = styled.div``;
 
 function App() {
-  <AppContainer></AppContainer>;
+  return (
+    <AppContainer>
+      <Header />
+      <ViewController />
+    </AppContainer>
+  );
 }
 
 export default App;
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,54 @@
+import { useContext } from 'react';
 import styled from 'styled-components';
+import logo from '../assets/logo.png';
+import { DataContext } from './context/Context';
+
+const HeaderContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #363c3c;
+  padding: 8px 20px;
+
+  .logo img {
+    height: 50px;
+  }
+
+  .search input {
+    font-size: 1rem;
+    padding: 12px;
+    border-radius: 10px;
+    width: 240px;
+  }
+`;
 
 function Header() {
-  return <div>Header</div>;
+  const { filters, setFilters } = useContext(DataContext);
+
+  function handleSearchChange(e) {
+    setFilters({
+      ...filters,
+      search: e.target.value,
+    });
+  }
+
+  return (
+    <HeaderContainer>
+      <div className="logo">
+        <img src={logo} alt="Helpful Human Logo" />
+      </div>
+      <div className="search">
+        <input
+          type="text"
+          name="search-bar"
+          id="search-bar"
+          value={filters.search}
+          onChange={handleSearchChange}
+          placeholder="Search"
+        />
+      </div>
+    </HeaderContainer>
+  );
 }
 
 export default Header;
