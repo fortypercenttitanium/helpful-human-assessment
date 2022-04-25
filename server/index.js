bdd56d835b076471f80322c162d7064ba184b44e
Alex Younger <alexanderyounger@gmail.com>
2022-04-25 07:41:32 -0400

    add cors, db router 404

--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
 require('dotenv').config();
 const express = require('express');
-const app = express();
+const cors = require('cors');
 const db = require('./db/db');
 const dbRouter = require('./routes/dbRouter');
+const app = express();
 
 const PORT = process.env.PORT || 8080;
+
+app.use(cors());
 
 app.use('/db', dbRouter);
 
--- server/routes/dbRouter.js
+++ server/routes/dbRouter.js
@@ -50,4 +50,6 @@ router.get('/color/:id', async (req, res) => {
   }
 });
 
+router.use('*', (req, res) => res.status(404).send('Resource not found'));
+
 module.exports = router;
