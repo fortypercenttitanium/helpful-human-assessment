5746273c1b3b9b9543a4b99afeb6573e61c0a657
Alex Younger <alexanderyounger@gmail.com>
2022-04-25 08:12:37 -0400

    fix server 404 bug

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 8080;
 app.use(cors());
 
 app.use('/db', dbRouter);
-app.use('*', res.status(404).send('Route not found'));
+app.use('*', (req, res) => res.status(404).send('Route not found'));
 
 app.listen(PORT, async () => {
   await db();
