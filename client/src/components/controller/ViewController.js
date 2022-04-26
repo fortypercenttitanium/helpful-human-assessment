87cab8304e2ab7753ebb57fd23aaa597801cc8a7
Alex Younger <alexanderyounger@gmail.com>
2022-04-26 16:41:16 -0400

    Move loading component, add error component

--- client/src/components/controller/ViewController.js
+++ client/src/components/controller/ViewController.js
@@ -2,12 +2,13 @@ import { useContext } from 'react';
 import { DataContext } from '../context/Context';
 import DetailView from '../layouts/DetailView';
 import Loading from '../Loading';
+import ErrorState from '../ErrorState';
 import SelectorView from '../layouts/SelectorView';
 
 function ViewController() {
   const { selectedColor, loading, inErrorState } = useContext(DataContext);
 
-  if (inErrorState) return <div>Error!</div>;
+  if (inErrorState) return <ErrorState />;
   if (loading) return <Loading />;
 
   return selectedColor ? <DetailView /> : <SelectorView />;
